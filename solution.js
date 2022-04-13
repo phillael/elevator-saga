5494324ace5c5091a7e33ec67783815e6f1df630
Phillip Aelony <phillael@yahoo.com>
2022-04-13 17:51:26 -0500

    Challenge #2 solution

--- solution.js
+++ solution.js
@@ -1,20 +1,48 @@
 {
     init: (elevators, floors) => {
-        
+
         console.clear(); // Get rid of the clutter!
         // Let's look at the objects we're working with
         console.log(elevators);
         console.log(floors);
-        
+
         const elevator = elevators[0]; // Let's use the first elevator
+        
+        elevator.on("floor_button_pressed", (floorNum) => {
+            // Go to floor when floor button pressed
+            elevator.goToFloor(floorNum);
+        });
+        
+        elevator.on("passing_floor", (floorNum, direction) => {
+            // pick people up if passing in the right direction
+            // console.log(floors[floorNum].buttonStates);
+            if (direction === "up" && floors[floorNum].buttonStates.up === "activated") {
+                
+                elevator.goToFloor(floorNum, true); // Do it before anything else
+                console.log(`stopping at passing floor ${floorNum} heading ${direction}`);
+                
+                // Send elevator if someone requests to go up
+                // This probably isn't the best place to do this...
+                floors.forEach(floor => floor.on("up_button_pressed", () => elevator.goToFloor(floor.level)));
+            }
+            if (direction === "down" && floors[floorNum].buttonStates.down === "activated") {
+                
+                elevator.goToFloor(floorNum, true) 
+                console.log(`stopping at passing floor ${floorNum} heading ${direction}`)
+                
+                // Send elevator if someone requests to go down
+                floors.forEach(floor => floor.on("down_button_pressed", () => elevator.goToFloor(floor.level)));
+            }
+        });
+
 
         // Whenever the elevator is idle (has no more queued destinations) ...
         elevator.on("idle", () => {
-            // let's go to all the floors
-            floors.forEach(floor => elevator.goToFloor(floor.level));
-        });
+            // Return to the lobby
+            elevator.goToFloor(0)
+        });             
     },
     update: (dt, elevators, floors) => {
-        // We normally don't need to do anything here
-    }
+            // We normally don't need to do anything here
+        }
 }
