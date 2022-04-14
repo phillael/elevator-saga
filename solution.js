58f920b64cb40bd48dbe58ccd193c2fa5f46b031
Phillip Aelony <phillael@yahoo.com>
2022-04-14 15:48:28 -0500

    Challenge #5 solution

--- solution.js
+++ solution.js
@@ -19,12 +19,24 @@
                 elevator.checkDestinationQueue();
                 // Note that you need to call checkDestinationQueue() for the change to take effect immediately.
                 
-                console.log(`elevator ${index} queue: ${elevator.destinationQueue}`);
+                // console.log(`elevator ${index} queue: ${elevator.destinationQueue}`);
+            }
+            
+            const setIndicators = () => {
+                
+                if (elevator.destinationDirection() === "up" || elevator.currentFloor() === 0) {
+                    elevator.goingUpIndicator(true);
+                    elevator.goingDownIndicator(false);
+                }
+                if (elevator.destinationDirection() === "down" || elevator.currentFloor() === topFloor) {
+                    elevator.goingDownIndicator(true);
+                    elevator.goingUpIndicator(false);
+                }
             }
 
             elevator.on("floor_button_pressed", (floorNum) => {
                 // Go to floor when floor button pressed
-                elevator.goToFloor(floorNum);      
+                elevator.goToFloor(floorNum);  
             });
 
             elevator.on("passing_floor", (floorNum, direction) => {
@@ -32,40 +44,32 @@
                 // console.log(floors[floorNum].buttonStates);
                 // console.log(elevator.loadFactor()); // load factor doesn't seem to be a problem yet.
                 
-                if (direction === "up" && floors[floorNum].buttonStates.up === "activated") {
-                    elevator.goToFloor(floorNum, true); // Do it before anything else
-                    // console.log(`elevator ${index} stopping at passing floor ${floorNum} heading ${direction}`);
+                if (elevator.loadFactor() < .6 ) {
+                    if (direction === "up" && floors[floorNum].buttonStates.up === "activated") {
+                        elevator.goToFloor(floorNum, true); // Do it before anything else
+                        // console.log(`elevator ${index} stopping at passing floor ${floorNum} heading ${direction}`);
+                    }
+
+                    if (direction === "down" && floors[floorNum].buttonStates.down === "activated") {
+                        elevator.goToFloor(floorNum, true);
+                        // console.log(`elevator ${index} stopping at passing floor ${floorNum} heading ${direction}`);
+                    }
                 }
                 
-                if (direction === "down" && floors[floorNum].buttonStates.down === "activated") {
-                    elevator.goToFloor(floorNum, true);
-                    // console.log(`elevator ${index} stopping at passing floor ${floorNum} heading ${direction}`);
-                }
-                
+                setIndicators();
                 updateDestinationQueue();
-                // Calling this here should keep the queue updated
             });
 
             // Whenever the elevator is idle (has no more queued destinations) ...
-            elevator.on("idle", () => {
-                // Split elevators between lobby and topFloor
-                index % 2 === 0 ? elevator.goToFloor(0) : elevator.goToFloor(topFloor);
-            });  
-
-            floors.forEach(floor => {
-                floor.on("up_button_pressed", () => {
-                    // Add to the queue if the elevator is heading in that direction
-                    elevator.destinationDirection() === "up" && elevator.goToFloor(floor.level); 
-                          
-                    // console.log(`floor ${floor.level} has requested an elevator heading up`);
-                });
-                floor.on("down_button_pressed", () => {
-                    elevator.destinationDirection() === "down" && elevator.goToFloor(floor.level); 
-                    
-                    // console.log(`floor ${floor.level} has requested an elevator heading down`);
-                });
+            elevator.on("idle", () => {   
+                elevator.goToFloor(0);
+                // this seems to work better than spliting between floors
             });
-        });   
+            
+            elevator.on("stopped_at_floor", function(floorNum) {
+                setIndicators();
+            })            
+        });         
     },
     update: (dt, elevators, floors) => {
         // We normally don't need to do anything here
