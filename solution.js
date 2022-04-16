1f024bff34a96273381e4a042272c365dfa8abda
Phillip Aelony <phillael@yahoo.com>
2022-04-15 23:33:15 -0500

    Spinning elevators upgrade

--- solution.js
+++ solution.js
@@ -6,31 +6,35 @@
             challenge.options.spawnRate = 100;
             challenge.options.elevatorCapacities = [15];
         });
-        
+
         elevators.forEach((elevator, index) => { 
             elevator.on("idle", () => elevator.goToFloor(index));
             elevator.on("floor_button_pressed", (floorNum) => elevator.goToFloor(floorNum));
         });
     },
-    update: (dt, elevators, floors) => {
-        
-        const elevatorElems = document.getElementsByClassName('elevator');
-        const users = document.getElementsByClassName('user');      
-        
-        const randomColor = () => {
-            const r = Math.random() * 255;
-            const g = Math.random() * 255;
-            const b = Math.random() * 255; 
+        update: (dt, elevators, floors) => {
             
-            return `rgb(${r}, ${g}, ${b}`;
-        };
+            const elevatorElems = document.getElementsByClassName('elevator movable');
+            const users = document.getElementsByClassName('user');      
+            let rotation = 0;
             
-        for (let el of elevatorElems) {               
-            el.style.backgroundColor = randomColor();       
-        };      
+            const randomColor = () => {
+                
+                const r = Math.random() * 255;
+                const g = Math.random() * 255;
+                const b = Math.random() * 255; 
+
+                return `rgb(${r}, ${g}, ${b}`;
+            };           
             
-        for (let u of users) {                
-            u.style.color = randomColor();
-        };
-    }
+            for (let el of elevatorElems) {                 
+                el.style.backgroundColor = randomColor();   
+                el.style.transform = `${el.style.transform} rotate(${rotation}deg)`
+                rotation += 5;
+            };      
+
+            for (let u of users) {                
+                u.style.color = randomColor();
+            };
+        }
 }
